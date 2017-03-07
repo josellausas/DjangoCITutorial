30cda33dbe3c31ff51b5d625a69e13bec9a934ce
Zunware <jose@zunware.com>
2017-03-07 12:35:16 -0600

    Added Leads app

--- src/settings.py
+++ src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'src.apps.tasks',
+    'src.apps.leads'
 ]
 
 MIDDLEWARE = [
--- src/test_general.py
+++ src/test_general.py
@@ -4,20 +4,28 @@ import os
 from django.test import Client
 from django.test import TestCase
 
+from src import settings
+
 print('Hello Wolrd')
 
-class SiteIsUpCase(TestCase):
+class SiteCanRun(TestCase):
 
     def setUp(self):
         self.client = Client()
 
+    def test_has_leads_app_installed(self):
+        from src.apps import tasks
+        self.assertIsNotNone(tasks)
+        self.assertTrue('src.apps.tasks' in settings.INSTALLED_APPS)
 
-    def test_site_is_up(self):
-        # Get the index
+    def test_has_tasks_app_installed(self):
+        from src.apps import leads
+        self.assertIsNotNone(leads)
+        self.assertTrue('src.apps.leads' in settings.INSTALLED_APPS)
+
+    def test_site_is_up_and_has_index(self):
         response = self.client.get('/')
-
         # Expect an OK
         self.assertEqual(response.status_code, 200)
-
         # Should have some html tags
         self.assertContains(response, '<html>')
