ecc9736b2b0f40710fbaf1ab3c67585cbe0e0154
Zunware <jose@zunware.com>
2017-03-07 12:21:21 -0600

    All tests passing for hello world!

--- src/apps/tasks/views.py
+++ src/apps/tasks/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # The index view
 def index(request):
-    response = "<p>Hola Mundo!</p>"
+    response = "<html><p>Hola Mundo!</p></html>"
     return HttpResponse(response)
